7716e2d65f19213f066669632be035eadc8ad50b
João Pedro Megid Carrilho <jpcarrilhow@gmail.com>
2018-10-05 15:14:07 -0300

    Project 1 of the nanodegree, first commit

---
+++ p1_navigation/model.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class QNetwork(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, seed):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+        """
+        super(QNetwork, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.fc1 = nn.Linear(state_size, 256)
+        self.fc2 = nn.Linear(256, 128)
+        self.fc3 = nn.Linear(128, 64)
+        self.fc4 = nn.Linear(64, 32)
+        self.fc5 = nn.Linear(32, action_size)
+
+    def forward(self, state):
+        """Build a network that maps state -> action values."""
+        state = self.fc1(state)
+        state = F.relu(state)
+        state = self.fc2(state)
+        state = F.relu(state)
+        state = self.fc3(state)
+        state = F.relu(state)
+        state = self.fc4(state)
+        state = F.relu(state)
+        state = self.fc5(state)
+
+        return state
